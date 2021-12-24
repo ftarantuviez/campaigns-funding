959f7ac99debe7b00730a283e47ce28fcee45d73
Francisco Tarantuviez <tarantuviezf@gmail.com>
2021-12-24 17:45:23 -0300

    first commit

---
+++ ethereum/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+  "priority march mass review achieve into fence idea fantasy school machine chunk",
+  "https://rinkeby.infura.io/v3/a5baeec841194a21bcfc8e0182e77504"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
